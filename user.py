d7da195b89609b892a3d74eee9b133589785c6a1
mtashani <tahani@outlook.com>
2024-01-09 01:35:30 +0330

    Refactor code to randomize configurations in UserView class

--- user.py
+++ user.py
@@ -344,11 +344,11 @@ class UserView(FlaskView):
                     pass
         if randomize:
             configs = resp.split('\n')
-            if len(configs) > 2:
-                first_config = configs[0]
-                rest_configs = configs[1:]
-                random.shuffle(configs)
-                configs = [first_config] + rest_configs
+            if len(configs) > 3:
+                first_configs = configs[0:1]
+                rest_configs = configs[3:]
+                random.shuffle(rest_configs)
+                configs = first_configs+ rest_configs
             resp = '\n'.join(configs)
         if base64:
             resp = do_base_64(resp)
