a75ca0eea10e688a8e432d30b63a914091aa574e
mtashani <tahani@outlook.com>
2024-01-19 21:06:46 +0330

    update error message bulkUsers

--- resources.py
+++ resources.py
@@ -62,7 +62,7 @@ class UserResource(Resource):
 class bulkUsers(Resource):
     decorators = [hiddify.super_admin]
 
-    #def get(self):
+    # def get(self):
     #    uuid_list  = request.json
     #    users = User.query.filter(User.uuid.in_(uuid_list)).all()
     #    return jsonify([user.to_dict() for user in users])
@@ -74,7 +74,7 @@ class bulkUsers(Resource):
         users = request.json
         hiddify.bulk_register_users(users)
         for newuser in users:
-            user = user_by_uuid(newuser['uuid']) or abort(502, "unknown issue! user is not added")
+            user = user_by_uuid(newuser['uuid']) or abort(202, f"cant find user{newuser['uuid']}")
             user_driver.add_client(user)
         hiddify.quick_apply_users()
 
